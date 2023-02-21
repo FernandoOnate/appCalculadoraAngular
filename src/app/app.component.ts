e64944c8b2029175e8c1a27870096d4db890db87
Fernando Oñate Rodriguez <ofertar.fernando@gmail.com>
2023-02-20 19:12:59 -0500

    - Añadido los inputs - App hecha en el primer intento, a espera de correcciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   tituloApp = 'Aplicación de calculadora';
+  operandoA = 0;
+  operandoB = 0;
+  
+  resultado = 0;
+  sumarElementos(): void {
+    this.resultado = this.operandoA + this.operandoB;
+  }
 }
